9c81c4d912d50db6287e6c0989f840097dac5915
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-05-01 09:57:53 +0600

    homepage fixed and css added

--- controller/patient/homepageControl.php
+++ controller/patient/homepageControl.php
@@ -1,21 +1,13 @@
 <?php
-include '../models/mydb.php';
+include '../../model/patient_db.php';
 session_start();
 if(isset($_REQUEST['showProfile'])){
     $mydb = new Model();
     $conObj = $mydb->OpenCon();
     $result = $mydb->ShowProfile($conObj, $_SESSION['email']);
-    if($result->num_rows>0)
-    {
-        while($row = $result->fetch_assoc()){
-            
-          echo "<table><tr><td>Name:</td><td>".$row["name"]."</td></tr>";
-          echo "<tr><td>Date of Birth:</td><td>".$row["dob"]."</td></tr>";
-          echo "<tr><td>Phone Number:</td><td>".$row["phone"]."</td></tr>";
-          echo "<tr><td>Email:</td><td>".$row["email"]."</td></tr></table>"; 
-        }
-    }
-  }
+      echo $result["name"] ;
+  
+}
 
 if(isset($_REQUEST['updateProfile'])){
     header("Location: ../controller/updateControl.php");
@@ -23,4 +15,5 @@ if(isset($_REQUEST['updateProfile'])){
 if(isset($_REQUEST['documents'])){
   header("Location: ../controller/documentsControl.php");
 }
+
 ?>
