9d164582c77e451f9095a556f637beeb34bd311e
NishatAfla <nishatafla21@gmail.com>
2024-05-02 14:47:20 +0600

    otp_system

---
+++ view/doctor/send_otp.php
@@ -0,0 +1,19 @@
+<?php include '../../controller/doctor/forget_password.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Reset</title>
+    <link rel="stylesheet" href="../../css/doctor/style.css">
+</head>
+<body>
+    <div class="main">
+        <h1>Reset Password</h1>
+        <form action="" method="POST">
+            <label for="otp">OTP:</label>
+            <input type="text" id="otp" name="otp" placeholder="Enter your OTP" required>
+            <p class="wrap"><?php echo $otpError ?></p>
+            <div class="wrap"><input type="submit" value="Submit" name="otp_submit"></div>
+        </form>
+    </div>
+</body>
+</html>
