b0bbab1df75e46b37781b63695b31d2bc71d6510
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-05-09 21:25:28 +0600

    receipt pdf

--- js/patient_myscript.js
+++ js/patient_myscript.js
@@ -95,10 +95,11 @@ function demo(d_id) {
 function payment(app_id) {
   document.location.href = '../../controller/patient/paymentControl.php?app_id=' + app_id + '&token=1';
 }
-function donwloadReceipt(){
+function donwloadReceipt() {
   const element = document.getElementById('receipt');
 
   html2pdf().from(element).save();
+
 }
 /*
 function loadDoc() {
