5ea93632b0bc13877eeff1f89a803b22a1f2abc8
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-05-13 09:43:46 +0600

    index page

--- view/index.php
+++ view/index.php
@@ -1,46 +1,117 @@
-<?php foreach ($appointmentData as $appointment): ?>
-        <tr>
-            <td><?php echo $appointment['app_id']; ?></td>
-            <td><?php echo $appointment['p_id']; ?></td>
-            <td><?php echo $appointment['p_name']; ?></td>
-            <td><?php echo $appointment['p_gender']; ?></td>
-            <td><?php echo $appointment['time']; ?></td>
-            <td><?php echo $appointment['note']; ?></td>
-            <td>
-             
-            <button type="submit" name="set_time" id="set_time">Set Time</button>
-           
-          
-            <button type="submit" name="decline" value="<?php echo $appointment['app_id'];?>">Decline</button>
-      </tr>
-        <?php endforeach; ?>
-    </table>
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/indexStyle.css">
+</head>
 
-    <h2>Appointment Approved List</h2>
-    <table border="1">
-        <tr>
-            <th>Appointment Id</th>
-            <th>Patient Id</th>
-            <th>Patient Name</th>
-            <th>Patient Gender</th>
-            <th>Appointed Time </th>
-            <th>Patient Note</th>
+<body>
 
-           
-        </tr>
-        
-        <?php foreach ($approvedData as $approve): ?>
-        <tr>
-            <td><?php echo $approve['app_id']; ?></td>
-            <td><?php echo $approve['p_id']; ?></td>
-            <td><?php echo $approve['p_name']; ?></td>
-            <td><?php echo $approve['p_gender']; ?></td>
-            <td><?php echo $approve['stime']; ?></td>
-            <td><?php echo $approve['note']; ?></td>
-            <td><button type="submit" name="message" value="<?php echo $approve['app_id'] ?>">Message</button></td>
-            
-             
-           
-      </tr>
-        <?php endforeach; ?>
+    <div class="header">
+        <a href="index.php" class="logo"><img src="../upload/logo.png" alt="HTML tutorial" style="width:60px;height:60px;"></a>
+        <a href="#default" class="logo">Online Healthcare</a>
+
+        <div class="header-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Find Doctors</a>
+            <a href="#contact">Contact</a>
+            <div class="dropdown">
+                <button class="dropbtn">Login
+                    <i class="fa fa-caret-down"></i>
+                </button>
+
+                <div class="dropdown-content">
+                    <a href="http://localhost/online_healthcare/view/patient/login.php">Patient</a>
+                    <a href="http://localhost/online_healthcare/view/doctor/login.php">Doctor</a>
+                    <a href="#">Medical <br>Assistant</a>
+                </div>
+            </div>
+            <a href="#about">About</a>
+        </div>
+
+    </div>
+
+    <div style="padding-left:20px">
+        <h2>Automatic Slideshow</h2>
+        <p>Change image every 2 seconds:</p>
+
+        <div class="slideshow-container">
+
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="../upload/logo.png" style="width:150px;height:150px;">
+                <div class="text">Caption Text</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 3</div>
+                <img src="../upload/doctorphotohasna@gmail.comdownload.jpg" style="width:150px;height:150px;">
+                <div class="text">Caption Two</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 3</div>
+                <img src="img_mountains_wide.jpg" style="width:100%">
+                <div class="text">Caption Three</div>
+            </div>
+
+        </div>
+        <br>
+
+        <div style="text-align:center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+
+        <script>
+            let slideIndex = 0;
+            showSlides();
+
+            function showSlides() {
+                let i;
+                let slides = document.getElementsByClassName("mySlides");
+                let dots = document.getElementsByClassName("dot");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {
+                    slideIndex = 1
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex - 1].style.display = "block";
+                dots[slideIndex - 1].className += " active";
+                setTimeout(showSlides, 2000); // Change image every 2 seconds
+            }
+        </script>
+    </div>
+    <div class="header">
+
+        <a href="index.php" class="logo"><img src="../upload/logo.png" alt="HTML tutorial" style="width:60px;height:60px;"></a>
+        <div class="header-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Find Doctors</a>
+            <a href="#contact">Contact</a>
+            <div class="dropdown">
+                <button class="dropbtn">Login
+                    <i class="fa fa-caret-down"></i>
+                </button>
+
+                <div class="dropdown-content">
+                    <a href="http://localhost/online_healthcare/view/patient/login.php">Patient</a>
+                    <a href="http://localhost/online_healthcare/view/doctor/login.php">Doctor</a>
+                    <a href="#">Medical <br>Assistant</a>
+                </div>
+            </div>
+            <a href="#about">About</a>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
